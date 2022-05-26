13183e3aed475e0e4302ea8d9a53f83fc691a891
ThanhSang <thanhsang6325@gmail.com>
2022-05-26 16:11:48 +0700

    load list user, chatfragment, chat

--- app/src/main/java/com/example/chatapp/HomeFragment.java
+++ app/src/main/java/com/example/chatapp/HomeFragment.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.chatapp.Model.User;
+import com.example.chatapp.ViewModel.UserAdapter;
 import com.example.chatapp.databinding.FragmentHomeBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,11 +24,17 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HomeFragment extends HandlerUser {
 
     FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
     User user = new User();
+    UserAdapter userAdapter;
+    List<User> userList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,11 +51,16 @@ public class HomeFragment extends HandlerUser {
         // Inflate the layout for this fragment
         FragmentHomeBinding binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        // TSang: Get list user
+        userList = new ArrayList<>();
+        userAdapter = new UserAdapter(userList);
+        binding.onlines.setAdapter(userAdapter);
+        binding.onlines.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        // End
 
         if(auth.getCurrentUser() == null){
             getChildFragmentManager().beginTransaction().replace(R.id.homepage, new LoginFragment()).commit();
         }else {
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
             DatabaseReference reference = database.getReference().child("users").child(auth.getUid());
             reference.child("status").setValue("Online");
             reference.addValueEventListener(new ValueEventListener() {
@@ -61,6 +76,28 @@ public class HomeFragment extends HandlerUser {
 
                 }
             });
+            // TSang: Get list user
+            reference = database.getReference().child("users");
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    userList.clear();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        if (!dataSnapshot.getKey().equals(auth.getUid())){
+                            User user = dataSnapshot.getValue(User.class);
+                            user.setUid(dataSnapshot.getKey());
+                            userList.add(user);
+                        }
+                    }
+                    userAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+            // End get list user
 
             binding.imgView.setOnClickListener(new View.OnClickListener() {
                 @Override
--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -10,7 +10,6 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
